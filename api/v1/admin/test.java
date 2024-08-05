ce6b00c84015587a16ab8413c2e3a5f9c3899ecb
zhouxiaoliang <leonzxl@163.com>
2024-08-05 12:04:39 +0800

    Create test.java

---
+++ api/v1/admin/test.java
@@ -0,0 +1,14 @@
+public class Student {
+ 
+//    成员属性
+    String name;
+    int age;
+
+  public void eatdf() {
+      String test="12312";
+        System.out.println("吃饭");
+    }
+ 
+    public void sleepdfdf() {
+        System.out.println("睡觉");
+    }}
