2950dade69ad2cea45670383507cc8fddc84c935
zhouxiaoliang <leonzxl@163.com>
2024-08-08 20:28:46 +0800

    Update test.java

--- api/v1/admin/test.java
+++ api/v1/admin/test.java
@@ -4,7 +4,7 @@ public class Student {
     String name;
     int age;
 
-  public void aaafffazzddeeatdf() {
+  public void aaafffazzddeefatdf() {
       String test="12312";
         System.out.println("吃饭");
     }
