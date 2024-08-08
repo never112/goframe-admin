d77b5e62c1df131d0f9db30a409aa9c0df12f1fe
zhouxiaoliang <leonzxl@163.com>
2024-08-08 10:16:24 +0800

    Update test.java

--- api/v1/admin/test.java
+++ api/v1/admin/test.java
@@ -8,7 +8,7 @@ public class Student {
       String test="12312";
         System.out.println("吃饭");
     }
- public void AaAaaAea_milks() {
+ public void AaAaaAea_milffks() {
       String test="12312";
         System.out.println("吃饭");
     }
