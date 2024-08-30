12673afe78be02db216e9ba05957cf37d5fe3312
zhouxiaoliang <leonzxl@163.com>
2024-08-30 14:36:48 +0800

    Update test.java

--- api/v1/admin/test.java
+++ api/v1/admin/test.java
@@ -16,3 +16,22 @@ public class Student {
     public void sleepdfdf() {
         System.out.println("睡觉");
     }}
+ public void AaAaszdzaAea_milffks() {
+      String test="12312";
+        System.out.println("吃饭");
+    }
+ 
+    public void slede23pdfdf() {
+        System.out.println("睡觉");
+    }}
+    public void sleepdfdf() {
+        System.out.println("睡觉");
+    }}
+ public void AaAaszgdzaAea_milffks() {
+      String test="12312";
+        System.out.println("吃饭");
+    }
+ 
+    public void sledfgfepdfdf() {
+        System.out.println("睡觉");
+    }}
