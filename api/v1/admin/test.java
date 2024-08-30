a9e622c308eff192baf5d2f97e231a00809ccf84
zhouxiaoliang <leonzxl@163.com>
2024-08-30 14:51:57 +0800

    Update test.java

--- api/v1/admin/test.java
+++ api/v1/admin/test.java
@@ -4,34 +4,8 @@ public class Student {
     String name;
     int age;
 
-  public void aassggaffffafffdzzdfdffddfhjdeedfatdf() {
+  public void aaslovsggaffffafffdzzdfdffddfhjdeedfatdf() {
       String test="12312";
         System.out.println("吃饭");
     }
- public void AaAaszzaAea_milffks() {
-      String test="12312";
-        System.out.println("吃饭");
-    }
- 
-    public void sleepdfdf() {
-        System.out.println("睡觉");
-    }}
- public void AaAaszdzaAea_milffks() {
-      String test="12312";
-        System.out.println("吃饭");
-    }
- 
-    public void slede23pdfdf() {
-        System.out.println("睡觉");
-    }}
-    public void sleepdfdf() {
-        System.out.println("睡觉");
-    }}
- public void AaAaszgdzaAea_milffks() {
-      String test="12312";
-        System.out.println("吃饭");
-    }
- 
-    public void sledfgfepdfdf() {
-        System.out.println("睡觉");
-    }}
+}
